fa9b77237a7480c26aa2880d013ef68f77ea8740
pauloemmilio <paulo.emilio@dce.ufpb.br>
2018-02-20 22:35:55 -0300

    Realizando requisições ao servidor

--- src/main/java/com/paulo/cobranca/controller/TituloController.java
+++ src/main/java/com/paulo/cobranca/controller/TituloController.java
@@ -2,12 +2,22 @@ package com.paulo.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.paulo.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroDeTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		System.out.println(titulo.getDescricao());
 		return "CadastroDeTitulo";
 	}
 }
